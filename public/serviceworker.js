b59cfe0cf7c9e09f51884994824b6bdc08763e68
Balaji Janarthanan <balajijanarthanan1997@gmail.com>
2021-02-20 08:51:48 +0530

    service worker fix

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -8,6 +8,7 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log("Opened cache");
+
       return cache.addAll(urlsToCache);
     })
   );
@@ -17,9 +18,7 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => {
-        caches.match("offline.html");
-      });
+      return fetch(event.request).catch(() => caches.match("offline.html"));
     })
   );
 });
